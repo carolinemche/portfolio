31203281de89f131809030f978b1292843a0003e
cmche <carolinecpink@gmail.com>
2020-05-05 00:26:47 -0400

    accordian page made

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getRandom(max){
 //random color selector for name of opening page
 let bigNameColors = ['#eb4335', '#fcc0d0']
 let randomN = getRandom(bigNameColors.length);
-document.getElementById('bigName').style.color = "" + bigNameColors[randomN]+ "";
+document.getElementById('bigName').style.color = " " + bigNameColors[randomN]+ " ";
 
 //get welcome text corresponding color depneding on opening header color
 if (bigNameColors[randomN] == "#eb4335"){
---
+++ script2.js
@@ -0,0 +1,15 @@
+//accordian javascript code to hide/show: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
