2a311b10cb997cd5ee0613af0a0424ba3bb55754
cmche <carolinecpink@gmail.com>
2020-05-07 11:31:07 -0400

    content added

---
+++ work.js
@@ -0,0 +1,12 @@
+const works = {
+  smallDataSquad: {
+    id: 'voogle',
+    title: 'Voogle',
+    date: 'Nov 2019 - Ongoing',
+    description: 'Voogle is a platform created on P5.js and hosted on Netlify where users can use keywords to search up specific Vines.',
+    website: [
+      'Access it at <a href="https://voogle.netlify.com/">Netlify</a>',
+      '<a href="https://github.com/carolinemche/Creative-Coding-Final ">Github Code</a>',
+    ],
+  }
+}
